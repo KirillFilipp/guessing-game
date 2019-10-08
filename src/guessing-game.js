9887e342415e53799f61b92bcfa9afe25c7841ad
KirillFilipp <kirill_filipp@mail.ru>
2019-10-09 00:51:33 +0300

    feat: add solution;

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.guessOne = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessOne = Math.round((this.min + this.max) / 2);
+        return this.guessOne;
     }
 
     lower() {
-
+        this.max = this.guessOne;
     }
 
     greater() {
-
+        this.min = this.guessOne;
     }
 }
 
